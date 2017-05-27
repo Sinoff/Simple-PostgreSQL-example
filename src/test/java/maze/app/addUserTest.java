401032ede1cd84906079e7764f951def00748144
Roi <sinoff@campus.technion.ac.il>
2017-05-27 11:31:57 +0300

    update user tests

--- src/test/java/maze/app/addUserTest.java
+++ src/test/java/maze/app/addUserTest.java
@@ -14,29 +14,25 @@ import static org.junit.Assert.*;
 /**
  * Created by rssinoff on 5/25/2017.
  */
-public class UsersTests {
+public class addUserTest {
 
     @BeforeClass
-    public static void createTables()
-    {
+    public static void createTables() {
         Solution.createTables();
     }
 
     @AfterClass
-    public static void dropTables()
-    {
+    public static void dropTables() {
         Solution.dropTables();
     }
 
     @Before
-    public void clearTables()
-    {
+    public void clearTables() {
         Solution.clearTables();
     }
 
     @Test
-    public void addUserTest()
-    {
+    public void addUserTest() {
         ReturnValue returnValue;
 
         int id_negative1 = -1;
@@ -143,54 +139,5 @@ public class UsersTests {
         returnValue = Solution.addUser(userReversedHop);
         assertNotNull(returnValue);
         assertEquals(OK, returnValue);
-    }
-
-    @Test
-    public void getUserTest()
-    {
-        ReturnValue returnValue;
-        User returnUser;
-        int id1 = 1;
-        int source1 = 1;
-        int destination2 = 2;
-        int id2 = 2;
-        int id3 = 3;
-
-        // add Hop
-        Hop testHop1 = new Hop(source1, destination2);
-        returnValue = Solution.addHop(testHop1);
-        assertNotNull(returnValue);
-        assertEquals(OK, returnValue);
-
-        // add Users
-        User user1 = new User(id1, source1, destination2);
-        returnValue = Solution.addUser(user1);
-        assertNotNull(returnValue);
-        assertEquals(OK, returnValue);
-        User user2 = new User(id2, source1, destination2);
-        returnValue = Solution.addUser(user2);
-        assertNotNull(returnValue);
-        assertEquals(OK, returnValue);
-
-        // #1 - OK
-        // get users which were added
-        returnUser = Solution.getUser(id1);
-        assertNotNull(returnUser);
-        assertEquals(returnUser, user1);
-        returnUser = Solution.getUser(id2);
-        assertNotNull(returnUser);
-        assertEquals(returnUser, user2);
-
-        // #2 - badUser
-        // get user which was never added
-        returnUser = Solution.getUser(id3);
-        assertNotNull(returnUser);
-        assertEquals(returnUser, User.badUser);
-    }
-
-    @Test
-    public void updateUserTest()
-    {
-
     }
 }
